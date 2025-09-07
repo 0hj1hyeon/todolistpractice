325b6b5e9b7937430492f85b948e9df756293f87
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-09-07 11:32:55 +0900

    start

---
+++ todolistpractice/todolistpractice/src/main/java/com/example/todolistpractice/TodolistpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistpracticeApplication.class, args);
+	}
+
+}
---
+++ todolistpractice/todolistpractice/src/test/java/com/example/todolistpractice/TodolistpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
